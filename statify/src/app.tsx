70661d5971546d6f44ac61cb32d38d188badcab8
Alexander E Tomack <alexander.e.tomack@vanderbilt.edu>
2024-06-08 09:52:35 -0400

    playing with logo and auth

--- statify/src/app.tsx
+++ statify/src/app.tsx
@@ -1,12 +1,24 @@
 import styles from './css/app.module.scss'
 import React from 'react'
 
+
+const CLIENT_ID = "59ce9c1fe3c94dfdacedb9c57e229ad2"
+const REDIRECT_URI = "https://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+const [token, setToken] = useState("")
+
 class App extends React.Component<{}, {count: number}> {
   state = {
     count: 0,
   };
 
   stopConfettiTimeout: NodeJS.Timeout | null = null;
+
+useEffect(() => {
+  const hash = window.location.hash
+  let token = window.localStorage.getItem("token")
+}, [])
 
   onButtonClick = () => {
     this.setState((state) => {
@@ -21,6 +33,7 @@ class App extends React.Component<{}, {count: number}> {
       <div className={styles.container}>
         <div className={styles.title}>{"My Custom App!"}</div>
         <button className={styles.button} onClick={this.onButtonClick}>{"Count up"}</button>
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
         <div className={styles.counter}>{this.state.count}</div>
       </div>
     </>
